4b62c6be4dd03f3712a0d3aa17f3f1a9760d6322
k3w <33373189+k3rc@users.noreply.github.com>
2025-06-07 20:36:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.getElementById('recommend-btn').addEventListener('click', async () => {
-    const response = await fetch('https://ТВОЙ-BACKEND-URL/api/recommend?track_id=1');
+    const response = await fetch('g4-production.up.railway.app');
     const data = await response.json();
     document.getElementById('recommendations').innerText = 
         'Recommended tracks: ' + data.recommended_tracks.join(', ');
