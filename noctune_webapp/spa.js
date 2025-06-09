0b0c92618ccfbe04000ef63810d86eb3600b9c9d
k3w <33373189+k3rc@users.noreply.github.com>
2025-06-09 16:10:06 +0200

    Delete noctune_webapp directory

--- noctune_webapp/spa.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('nav ul li').forEach(link => {
-  link.addEventListener('click', () => {
-    document.querySelectorAll('.section').forEach(sec => sec.classList.remove('active'));
-    const section = document.getElementById(link.dataset.section);
-    if (section) section.classList.add('active');
-  });
-});
